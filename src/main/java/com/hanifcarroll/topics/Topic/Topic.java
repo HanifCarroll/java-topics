5eccec8c8b58d3587afd5af90ca3cd09768b26cd
Hanif Carroll <hanifcarroll@gmail.com>
2018-12-08 14:53:56 -0500

    Add Topic entity

--- src/main/java/com/hanifcarroll/topics/Topic/Topic.java
+++ src/main/java/com/hanifcarroll/topics/Topic/Topic.java
@@ -2,10 +2,58 @@ package com.hanifcarroll.topics.Topic;
 
 import com.hanifcarroll.topics.BaseEntity;
 
-import javax.persistence.Entity;
-import javax.persistence.Table;
+import javax.persistence.*;
+import java.util.HashSet;
+import java.util.Set;
 
 @Entity
 @Table(name = "topic")
 public class Topic extends BaseEntity {
+
+    @Column(name = "title")
+    private String title;
+
+    @Column(name = "description")
+    private String description;
+
+    @Column(name = "author")
+    private String author;
+
+    @OneToMany(cascade = CascadeType.ALL, mappedBy = "topic")
+    private Set<Comment> comments = new HashSet<>();
+
+    public Topic() {
+    }
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+
+    public Set<Comment> getComments() {
+        return comments;
+    }
+
+    public void setComments(Set<Comment> comments) {
+        this.comments = comments;
+    }
 }
