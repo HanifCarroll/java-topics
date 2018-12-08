95946d9a57e4b94d4b2881a72884c620d3aba3c9
Hanif Carroll <hanifcarroll@gmail.com>
2018-12-08 14:48:13 -0500

    Add BaseEntity class.

---
+++ src/main/java/com/hanifcarroll/topics/BaseEntity.java
@@ -0,0 +1,49 @@
+package com.hanifcarroll.topics;
+
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
+import javax.persistence.*;
+import java.util.Date;
+
+@MappedSuperclass
+public class BaseEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    public Long id;
+
+    @CreationTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "created_at")
+    private Date createdAt;
+
+    @UpdateTimestamp
+    @Temporal(TemporalType.TIMESTAMP)
+    @Column(name = "updated_at")
+    private Date updatedAt;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public Date getCreatedAt() {
+        return createdAt;
+    }
+
+    public void setCreatedAt(Date createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    public Date getUpdatedAt() {
+        return updatedAt;
+    }
+
+    public void setUpdatedAt(Date updatedAt) {
+        this.updatedAt = updatedAt;
+    }
+}
+
---
+++ src/main/java/com/hanifcarroll/topics/Topic/Topic.java
@@ -0,0 +1,11 @@
+package com.hanifcarroll.topics.Topic;
+
+import com.hanifcarroll.topics.BaseEntity;
+
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+@Entity
+@Table(name = "topic")
+public class Topic extends BaseEntity {
+}
